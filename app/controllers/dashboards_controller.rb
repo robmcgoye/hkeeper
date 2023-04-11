a47085316cfc13848d82a1d1bb1e5a03b0155495
Rob McGoye <rob@mcgoye.com>
2023-04-11 16:37:13 -0400

    Dashboard layout and rust desk download

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,6 +2,7 @@ class DashboardsController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @accounts = policy_scope(Account)
   end
   
 end
--- app/models/computer.rb
+++ app/models/computer.rb
@@ -5,6 +5,6 @@ class Computer < ApplicationRecord
   validates_uniqueness_of :key, scope: :account_id
 
   scope :computers_in_account, -> (account_ids) { where(account_id: account_ids) }
-  
+  scope :not_contacted, -> { where("last_contacted_at isnull or last_contacted_at < ?", (Date.today - 15.days)) }
 
 end
